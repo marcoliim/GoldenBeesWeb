85749d89a0cda78e80b57597d891ee0a3b6c0cb2
Marco Lim <maarrcolim@gmail.com>
2021-03-10 17:22:19 +0700

    Second Commit

---
+++ script/carousel.js
@@ -0,0 +1,13 @@
+$(".page-content .testimonial .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    544: {
+      items: 2,
+    },
+  },
+});
